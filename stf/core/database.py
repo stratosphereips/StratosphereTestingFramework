fc04df5195613c2ab0c400175196947abb1ea32a
eldraco <eldraco@gmail.com>
2015-03-06 15:19:25 +0100

    Code to show info about the database

--- stf/core/database.py
+++ stf/core/database.py
@@ -74,4 +74,46 @@ class Database:
         self.db.pack
         self.db.close()
 
+    def info(self):
+        """ Info about the database"""
+        print_warning('Info about the root object of the database')
+        print_info('Main Branch | Len')
+        for mainbranches in self.root.keys():
+            print('\t{:12} | {}'.format(mainbranches, len(self.root[mainbranches])))
+        print_info('_p_changed (the persistent state of the object): {}'.format(self.root._p_changed))
+        print('\t-> None: The object is a ghost.\n\t-> False but not None: The object is saved (or has never been saved).\n\t-> True: The object has been modified since it was last saved.')
+        print_info('_p_state (object persistent state token): {}'.format('GHOST' if self.root._p_state == -1 else 'UPTODATE' if self.root._p_state == 0 else 'CHANGED' if self.root._p_state == 1 else 'STICKY'))
+        print_info('_p_jar: {}'.format(self.root._p_jar))
+        print_info('_p_oid (persistent object id): {}'.format(self.root._p_oid))
+        print
+        print_warning('Info about the database object ')
+        print_info('Database Name: {}.'.format(self.db.getName()))
+        print_info('Database Size: {} B ({} MB).'.format(self.db.getSize(), self.db.getSize()/1024/1024))
+        print_info('Object Count: {}.'.format(self.db.objectCount()))
+        print_info('Connection debug info: {}.'.format(self.db.connectionDebugInfo()))
+        print_info('Cache details:')
+        for detail in self.db.cacheDetail():
+            print_info('\t{}'.format(detail))
+
+    def revert(self):
+        """ revert the connection of the database to the previous state before the last pack"""
+        question=raw_input('Warning, this command sync the database on disk with the information on memory. Effectively erasing the changes that were not committed and bringing the new information committed by other instances. \n YES or NO?:')
+        if question == 'YES':
+            self.connection.sync()
+        else:
+            print_error('Not reverting.')
+
+    def pack(self):
+        """ Pack the database """
+        self.db.pack()
+
+    def commit(self):
+        """ Commit the changes in the connection to the database """
+        import transaction
+        transaction.commit()
+
+
+
+
+
 __database__ = Database()
