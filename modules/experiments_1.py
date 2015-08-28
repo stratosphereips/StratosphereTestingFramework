76c987614a52d397ad2f5820d992353875b8edc8
Sebastian Garcia <eldraco@gmail.com>
2015-08-28 09:37:47 +0200

    Change names of functions

--- modules/experiments_1.py
+++ modules/experiments_1.py
@@ -36,11 +36,14 @@ class Experiment(persistent.Persistent):
     def set_id(self, id):
         self.id = id
 
-    def set_name(self, name):
-        self.name = name
+    def set_description(self, description):
+        self.description = description
 
-    def get_name(self):
-        return self.name
+    def get_description(self):
+        return self.description
+
+    def delete(self):
+        return True
 
     # Mandatory __repr__ module. Something you want to identify each object with. Usefull for selecting objects later
     def __repr__(self):
@@ -69,7 +72,8 @@ class Group_of_Experiments(Module, persistent.Persistent):
         self.parser.add_argument('-l', '--list', action='store_true', help='List the Experiments.')
         # Example of a parameter with arguments
         self.parser.add_argument('-p', '--printstate', metavar='experiment_id', help='Print some info about the experiment.')
-        self.parser.add_argument('-g', '--generate', metavar='generate', help='Create a new object with a name. Give name.')
+        self.parser.add_argument('-n', '--new', action='store_true', help='Create a new experiment. Use -r to assign training models. Use -t to select a testing experiment.')
+        self.parser.add_argument('-d', '--delete', metavar='delete', help='Delete an experiment given the id.')
 
     def get_name(self):
         """ Return the name of the module"""
@@ -87,20 +91,21 @@ class Group_of_Experiments(Module, persistent.Persistent):
     ############ End of Mandatory Methods #########################
 
 
-    def get_object(self, id):
+    def get_experiment(self, id):
         return self.main_dict[id]
 
-    def get_objects(self):
+    def get_experiments(self):
         return self.main_dict.values()
 
-    def list_objects(self):
+    def list_experiments(self):
         print_info('List of objects')
         rows = []
-        for object in self.get_objects():
-            rows.append([ object.get_id(), object.get_name() ])
-        print(table(header=['Id', 'Name'], rows=rows))
+        for object in self.get_experiments():
+            rows.append([ object.get_id(), object.get_description() ])
+        print(table(header=['Id', 'Description'], rows=rows))
 
-    def create_new_object(self, name):
+    def create_new_experiment(self):
+        """ Create a new experiment """
         # Generate the new id
         try:
             new_id = self.main_dict[list(self.main_dict.keys())[-1]].get_id() + 1
@@ -108,12 +113,22 @@ class Group_of_Experiments(Module, persistent.Persistent):
             new_id = 1
         # Create the new object
         new_object = Experiment(new_id)
-        # Do something with it
-        new_object.set_name(name)
+        # Set the description
+        desc = raw_input("Description: ")
+        new_object.set_description(desc)
         # Store on DB
         self.main_dict[new_id] = new_object
 
-
+    def delete_experiment(self, id):
+        """ Deletes an experiment """
+        ans = raw_input('Are you sure you want to delete experiment {} (YES/NO)?: '.format(id))
+        if ans == "YES":
+            # Get the experiment
+            exp = self.get_experiment(id)
+            # First delete everything inside the experiment
+            exp.delete()
+            # Now delete it from the list of experiments
+            self.main_dict.pop(id)
 
     # The run method runs every time that this command is used. Mandatory
     def run(self):
@@ -139,9 +154,11 @@ class Group_of_Experiments(Module, persistent.Persistent):
 
         # Process the command line and call the methods. Here add your own parameters
         if self.args.list:
-            self.list_objects()
-        elif self.args.generate:
-            self.create_new_object(self.args.generate)
+            self.list_experiments()
+        elif self.args.new:
+            self.create_new_experiment()
+        elif self.args.delete:
+            self.delete_experiment(int(self.args.delete))
         else:
             print_error('At least one of the parameter is required in this module')
             self.usage()
