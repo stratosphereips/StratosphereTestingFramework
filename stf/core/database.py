bb666fd83a80f8a1ed4355e4037d4870c00f4917
eldraco <eldraco@gmail.com>
2015-09-11 15:56:37 +0200

    Do not list the amount of stuff

--- stf/core/database.py
+++ stf/core/database.py
@@ -101,8 +101,9 @@ class Database:
             return False
 
     def list(self):
-        for structure in self.root:
-            print_info('Amount of {} in the DB so far: {}'.format(structure, len(self.root[structure])))
+        #for structure in self.root:
+        #    print_info('Amount of {} in the DB so far: {}'.format(structure, len(self.root[structure])))
+        pass
 
     def delete_structure(self, structure_name):
         """ Delete a structure from the db """
