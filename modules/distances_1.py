d7a59c6cbeb059332ec845ca229790d0c90c7e13
eldraco <eldraco@gmail.com>
2015-10-07 20:47:56 +0200

    If the original model was deleted, delete the distnace
    - When listing distances

--- modules/distances_1.py
+++ modules/distances_1.py
@@ -593,6 +593,8 @@ class Group_of_Detections(Module, persistent.Persistent):
         for distance in self.get_distances():
             if self.apply_filter(distance):
                 regenerate = distance.check_need_for_regeneration()
+                if not regenerate:
+                    self.delete_distance(distance)
                 training_label = distance.get_training_label()
                 testing_label = distance.get_testing_label()
                 all_text += ' {:<4} | {:75} | {:75} | {:8.3f} ({:>5}) | {}\n'.format(distance.get_id(), distance.get_training_structure_name() + ': ' + str(distance.get_model_training_id()) + ' (' + training_label + ')', distance.get_testing_structure_name() + ': ' + str(distance.get_model_testing_id()) + ' (' + testing_label + ')', distance.get_distance(), distance.get_amount(), regenerate)
