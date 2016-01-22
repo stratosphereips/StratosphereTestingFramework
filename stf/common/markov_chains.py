1dd565019209763b58565a53ca1c47f1b64c07af
eldraco <eldraco@gmail.com>
2016-01-22 16:51:43 +0100

    Creation of the Markov Chain library for the Stratosphere Project
    - Our own library where we will implement 1st order and 2nd order markov
      chains.

---
+++ stf/common/markov_chains.py
@@ -0,0 +1,70 @@
+# This file is from the Stratosphere Testing Framework
+# See the file 'LICENSE' for copying permission.
+
+# Library to compute some markov chain functions for the Stratosphere Project. We created them because pykov lacked the second order markov chains
+
+class Matrix(dict):
+    """ The basic matrix object """
+    def __init__(self, *args, **kw):
+        super(Matrix,self).__init__(*args, **kw)
+        self.itemlist = super(Matrix,self).keys()
+
+    def walk_probability(self, states):
+        prob = 0
+        #for i in self:
+        #    print self[i]
+        return prob
+
+
+def maximum_likelihood_probabilities(states, order=1):
+    """ Our own second order Markov Chain implementation """
+    initial_matrix = {}
+    initial_vector = {}
+    total_transitions = 0
+    amount_of_states = len(states)
+    print 'Receiving {} states to compute the Markov Matrix of {} order'.format(amount_of_states, order)
+    # 1st order
+    if order == 1:
+        # Create matrix
+        index = 0
+        while index < amount_of_states:
+            state1 = states[index]
+            try:
+                state2 = states[index + 1]
+            except IndexError:
+                # The last state is alone. There is no transaction, forget about it.
+                break
+            try:
+                temp = initial_matrix[state1]
+            except KeyError:
+                # First time there is a transition FROM state1
+                initial_matrix[state1] = {}
+                initial_vector[state1] = 0
+            try:
+                value = initial_matrix[state1][state2]
+                initial_matrix[state1][state2] = value + 1
+            except KeyError:
+                # First time there is a transition FROM state 1 to state2
+                initial_matrix[state1][state2] = 1
+            initial_vector[state1] += 1
+            total_transitions += 1
+            # Move along
+            index += 1
+        # Normalize using the initial vector
+        #matrix = {}
+        matrix = Matrix()
+        init_vector = {}
+        for state1 in initial_matrix:
+            # Create the init vector
+            init_vector[state1] = initial_vector[state1] / float(total_transitions)
+            for state2 in initial_matrix[state1]:
+                value = initial_matrix[state1][state2]
+                initial_matrix[state1][state2] = value / float(initial_vector[state1])
+                # Change the style of the matrix
+                matrix[(state1,state2)] = initial_matrix[state1][state2]
+        #print init_vector
+        #for value in matrix:
+        #    print value, matrix[value]
+    return (init_vector, matrix)
+
+
